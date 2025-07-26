7af94e145a94a3651fab9872116e3e293af6437d
rte8 <19151063862@163.com>
2025-07-26 14:12:08 +0800

    清空仓库内容

--- UR5_多代理模型/TestRBF.m
+++
@@ -1,8 +0,0 @@
-function [T_sim] = TestRBF(pop,net,ps_input,ps_output)
-    P_test = pop';
-    %  数据归一化
-    p_test = mapminmax('apply', P_test, ps_input);
-    
-    t_sim = sim(net, p_test);
-    T_sim = mapminmax('reverse', t_sim, ps_output);
-end
